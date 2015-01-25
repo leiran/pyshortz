613cab27f722371551572f73941de7ec8c127c51
John OBrien <john.obrien@gmail.com>
2015-01-24 22:19:21 -0500

    Added in a blank file and directory structure for holding puzzle solutions.

---
+++ solutions/2015-01-18/two_animals.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 24, 2015
+
+Name two animals, both mammals, one domestic, the other wild, 
+put their letters together and rearrange to spell another 
+mammal, this one wild and not seen in North America.
+
+@author: John
+'''
+
+if __name__ == '__main__':
+    pass
