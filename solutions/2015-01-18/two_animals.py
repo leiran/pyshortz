bf47e63b067b9430333b749b40729c8e7b311535
John OBrien <john.obrien@gmail.com>
2015-01-29 21:09:48 -0500

    Tried making a list of some animals.

--- solutions/2015-01-18/two_animals.py
+++ solutions/2015-01-18/two_animals.py
@@ -8,5 +8,38 @@ mammal, this one wild and not seen in North America.
 @author: John
 '''
 
+
+def main():
+    wild_mammals = ['bison',
+                   'fox',
+                   'wolf',
+                   'buffalo',
+                   'hare',
+                   'rabbit',
+                   'squirrel',
+                   'camel',
+                   'moose']
+    domestic_mammals = ['cow',
+                        'cat',
+                        'dog',
+                        'pig',
+                        'sheep',
+                        'llama',
+                        ]
+    
+    alpha = []
+    for animal in wild_mammals:
+        alpha.append("".join(sorted(animal)))
+    
+    
+    for wild_mammal in wild_mammals:
+        for domestic_mammal in domestic_mammals:
+            candidate = sorted(wild_mammal.join(domestic_mammal))
+            if candidate in alpha:
+                print("{0} is a candidate")
+    print("Done.")
+
+
+
 if __name__ == '__main__':
-    pass
+    main()
